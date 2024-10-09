9a246733aedf7652449833a1564f12cd7abc6586
MatiasMiguelGomez <miguel_gomez30@outlook.com>
2024-10-09 15:34:51 -0300

    componente icon

---
+++ site/src/app/components/icon/types/IconTypes.ts
@@ -0,0 +1,15 @@
+export type Icon =
+  | "boxTick"
+  | "cartLarge"
+  | "cartSmall"
+  | "category"
+  | "delivery"
+  | "heart"
+  | "heartFilled"
+  | "minus"
+  | "plus"
+  | "search"
+  | "shop"
+  | "trash"
+  | "user"
+  | "whatsapp";
