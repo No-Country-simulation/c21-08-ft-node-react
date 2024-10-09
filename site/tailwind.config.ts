1bb296e19b16c2fb8013ecdf1a7587a42b188074
GhostOrder28 <jtrvic7@gmail.com>
2024-10-09 10:05:38 -0500

    cambios en configuración de tailwind, home en blanco

--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -7,10 +7,30 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens:{
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray100: '#F2F2F2',
+        gray200: '#EBEBEB',
+        gray300: '#D9D9D9',
+        gray400: '#C1C1C1',
       },
     },
   },
