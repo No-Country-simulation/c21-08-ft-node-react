1095eec51c4fc06f51e274cbba45313c91902e6c
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-09 14:31:13 -0300

    add: agregar mocks de 3 productos

---
+++ site/src/mocks/products.ts
@@ -0,0 +1,31 @@
+import { Product } from "@/app/types/ItemCarritoProps";
+
+export const products: Product[] = [
+    {
+        productID: 'P001',
+        name: 'Wireless Headphones',
+        description: 'High-quality noise-cancelling wireless headphones.',
+        price: 99.99,
+        imgUrl: '/images/product-placeholder.webp',
+        categoryID: 'C001',
+        stock: 50
+    },
+    {
+        productID: 'P002',
+        name: 'Smartwatch',
+        description: 'Stylish smartwatch with fitness tracking and notifications.',
+        price: 149.99,
+        imgUrl: '/images/product-placeholder.webp',
+        categoryID: 'C002',
+        stock: 30
+    },
+    {
+        productID: 'P003',
+        name: 'Gaming Mouse',
+        description: 'Ergonomic gaming mouse with customizable buttons and RGB lighting.',
+        price: 59.99,
+        imgUrl: '/images/product-placeholder.webp',
+        categoryID: 'C003',
+        stock: 100
+    }
+];
