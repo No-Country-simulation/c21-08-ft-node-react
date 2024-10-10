7cba4b111f7d322d9a5b43002acab6ca2f963a4e
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-10 09:44:49 -0300

    change: cambiar datos de los productos para representar mejor lo que vende la tienda

--- site/src/mocks/products.ts
+++ site/src/mocks/products.ts
@@ -3,27 +3,27 @@ import { Product } from "@/app/types/Product";
 export const products: Product[] = [
     {
         productID: 'P001',
-        name: 'Wireless Headphones',
-        description: 'High-quality noise-cancelling wireless headphones.',
-        price: 99.99,
+        name: 'Galletas de Chocolate',
+        description: 'Galletas crocantes con trozos de chocolate.',
+        price: 1999,
         imgUrl: '/images/product-placeholder.webp',
         categoryID: 'C001',
         stock: 50
     },
     {
         productID: 'P002',
-        name: 'Smartwatch',
-        description: 'Stylish smartwatch with fitness tracking and notifications.',
-        price: 149.99,
+        name: 'Papas Fritas',
+        description: 'Papas fritas crujientes con sal.',
+        price: 2199,
         imgUrl: '/images/product-placeholder.webp',
         categoryID: 'C002',
         stock: 30
     },
     {
         productID: 'P003',
-        name: 'Gaming Mouse',
-        description: 'Ergonomic gaming mouse with customizable buttons and RGB lighting.',
-        price: 59.99,
+        name: 'Gomitas de Frutas',
+        description: 'Gomitas dulces con sabor a frutas.',
+        price: 699,
         imgUrl: '/images/product-placeholder.webp',
         categoryID: 'C003',
         stock: 100
