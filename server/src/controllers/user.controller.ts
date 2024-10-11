bf5a792b8b9900e424b9da538d08fa5cf993c5cd
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-10-11 12:42:54 -0300

    refactorize some staff

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import { UserService } from "../service/user.service";
+import { UserService } from "../services/user.service";
 import { User } from "../entities/User.entity";
 
-const userService = new UserService();
-
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   async getAllUsers(req: Request, res: Response): Promise<any> {
     try {
-      const users: User[] = await userService.getAllUsers();
+      const users: User[] = await this.userService.getAllUsers();
       return res.json(users);
     } catch (error) {
       return res
---
+++ server/src/services/user.service.ts
@@ -0,0 +1,13 @@
+import { User } from "../entities/User.entity";
+import { userRepository } from "../repositories/userRepository";
+
+export class UserService {
+  async getAllUsers(): Promise<User[]> {
+    try {
+      return await userRepository.find();
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      throw new Error("Failed to fetch users");
+    }
+  }
+}
