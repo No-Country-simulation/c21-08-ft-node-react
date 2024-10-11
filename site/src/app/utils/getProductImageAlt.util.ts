a6fffa264ff84f5c8e985757708314e04273a1b6
GhostOrder28 <jtrvic7@gmail.com>
2024-10-11 17:56:06 -0500

    new utils function to manipulate strings

--- site/src/app/utils/getProductImageAlt.util.ts
+++ site/src/app/utils/getProductImageAlt.util.ts
@@ -1,3 +1,7 @@
 export const getProductImageAlt = (productTitle: string):string =>{
     return `Imagen de ${productTitle}`
 }
+
+export const strParseOut = (str: string) => {
+  return str.split("-").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
