b224957c20c726a766f93682a79d8947623f56a6
MatiasMiguelGomez <miguel_gomez30@outlook.com>
2024-10-11 13:31:26 -0300

    fix: agregue plugin de tailwind

--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
+import lineClamp from "@tailwindcss/line-clamp";
 
 const config: Config = {
   content: [
@@ -8,37 +9,37 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens:{
-      '2xl': {'max': '1535px'},
+    screens: {
+      "2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'xl': {'max': '1279px'},
+      xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      lg: { max: "1023px" },
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      sm: { max: "639px" },
       // => @media (max-width: 639px) { ... }
     },
     extend: {
       fontFamily: {
-        'sans': [ '"Raleway"', ...defaultTheme.fontFamily.sans ]
+        sans: ['"Raleway"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        gray100: '#F2F2F2',
-        gray200: '#EBEBEB',
-        gray300: '#D9D9D9',
-        gray400: '#C1C1C1',
-        gray1000: '#292D32',
+        gray100: "#F2F2F2",
+        gray200: "#EBEBEB",
+        gray300: "#D9D9D9",
+        gray400: "#C1C1C1",
+        gray1000: "#292D32",
       },
     },
   },
-  plugins: [],
+  plugins: [lineClamp],
 };
 export default config;
