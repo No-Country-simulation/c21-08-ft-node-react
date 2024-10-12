9cb3fff236b32621c3a2e756738634f04d2ecd70
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-12 12:57:07 -0300

    add: add overlay when cart opens

--- site/src/app/components/Cart/Cart.component.tsx
+++ site/src/app/components/Cart/Cart.component.tsx
@@ -18,18 +18,19 @@ const Cart = () => {
     return (
         <div id="carrito-btn">
             <button className="flex justify-end gap-2" onClick={changeCartVisibility}><Icon iconType="cartSmall"/><span>{productsInCart.length}</span></button>
-            <div className={`${isCartVisible ? 'flex' : 'hidden'} flex-col h-[calc(100vh-89.7px)] justify-between fixed right-0 top-[89.7px] bg-white px-8 py-4 shadow-xl w-[480px]`}>
-                <div className="flex flex-col gap-8">
-                    <CartHeader changeCartVisibility={changeCartVisibility} />
-                    <CartProductList 
-                        productsInCart={productsInCart} 
-                        incrementProductQty={incrementProductQty} 
-                        decrementProductQty={decrementProductQty} 
-                        removeProduct={removeProduct} 
-                    />
+            {isCartVisible && (<>
+                <div onClick={changeCartVisibility} className="w-dvw h-dvh left-0 top-[96px] opacity-30 z-[-1000] bg-black fixed"/>
+                <div className={`${isCartVisible ? 'flex' : 'hidden'} flex-col h-[calc(100vh-96px)] gap-4 fixed right-0 top-[96px] bg-white px-8 py-4 shadow-xl w-[480px]`}>
+                  <CartHeader changeCartVisibility={changeCartVisibility} />
+                  <CartProductList 
+                      productsInCart={productsInCart} 
+                      incrementProductQty={incrementProductQty} 
+                      decrementProductQty={decrementProductQty} 
+                      removeProduct={removeProduct} 
+                  />
+                  <CartFooter productsInCart={productsInCart} />    
                 </div>
-                <CartFooter productsInCart={productsInCart} />    
-            </div>
+            </>)}
         </div>
     )
 }
