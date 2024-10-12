23b01dc59b4ba8a94bd3c85b7109e833ecb23fef
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-12 13:29:16 -0300

    change: price styles

--- site/src/app/components/Price/Price.component.tsx
+++ site/src/app/components/Price/Price.component.tsx
@@ -6,9 +6,9 @@ const Price = ({price, size} : PriceProps) => {
     const {int, dec} = getIntAndDecSplited(price)
 
     return (
-        <p>
+        <p className={`flex items-center ${ size === "L" ? 'leading-8' : 'leading-4'}`}>
             <span className={`${ size === "L" ? 'text-[32px]' : 'text-[20px]' }`}>${int}</span>
-            <span className={`${ size === "L" ? 'text-[16px]' : 'text-[12px]' } `}>{dec}</span>
+            <span className={`${ size === "L" ? 'text-[16px] leading-4' : 'text-[12px] leading-3' }`}>{dec}</span>
         </p>
     ) 
 }
