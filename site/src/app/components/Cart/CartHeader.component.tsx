b7fa574a0105a89d07d71cd2c4ee8721605c23c6
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-12 13:29:05 -0300

    change: change cart text size

--- site/src/app/components/Cart/CartHeader.component.tsx
+++ site/src/app/components/Cart/CartHeader.component.tsx
@@ -4,7 +4,7 @@ import Icon from "../Icon/Icon.component"
 const CartHeader = ({ changeCartVisibility }: CartHeaderProps) => {
     return (
         <div className="flex cursor-pointer gap-2">
-            <h2>Tu carrito</h2>
+            <h2 className="text-2xl">Tu carrito</h2>
             <button onClick={changeCartVisibility}><Icon iconType="rightArrow" /></button>
         </div>
     )
