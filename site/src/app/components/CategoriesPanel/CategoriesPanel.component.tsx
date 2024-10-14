f1afd42271aea0806309bf46ae420dda9373d709
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-14 13:18:23 -0300

    change: url param changed from cat -> name

--- site/src/app/components/CategoriesPanel/CategoriesPanel.component.tsx
+++ site/src/app/components/CategoriesPanel/CategoriesPanel.component.tsx
@@ -11,14 +11,14 @@ const CategoriesPanel = ({ changeCategoriesVisibility }: CategoriesPanelProps) =
       />
       <div className='fixed left-0 top-[96px] h-[332px] w-full bg-white'>
         <nav className='mx-auto grid h-full max-w-[1000px] grid-cols-4 grid-rows-2 gap-5 py-5'>
-          <Category name='Almacén' route='/categoria?cat=almacén'><Icon iconType='category' /></Category>
-          <Category name='Congelados' route='/categoria?cat=congelados'><Icon iconType='category' /></Category>
-          <Category name='Librería' route='/categoria?cat=librería'><Icon iconType='category' /></Category>
-          <Category name='Vinoteca' route='/categoria?cat=vinoteca'><Icon iconType='category' /></Category>
-          <Category name='Limpieza' route='/categoria?cat=limpieza'><Icon iconType='category' /></Category>
-          <Category name='Bebidas sin alcohol' route='/categoria?cat=bebidas-sin-alcohol'><Icon iconType='category' /></Category>
-          <Category name='Bebidas con alcohol' route='/categoria?cat=bebidas-con-alcohol'><Icon iconType='category' /></Category>
-          <Category name='Variados' route='/categoria?cat=variados'><Icon iconType='category' /></Category>
+          <Category name='Almacén' route='/categoria?name=almacén'><Icon iconType='category' /></Category>
+          <Category name='Congelados' route='/categoria?name=congelados'><Icon iconType='category' /></Category>
+          <Category name='Librería' route='/categoria?name=librería'><Icon iconType='category' /></Category>
+          <Category name='Vinoteca' route='/categoria?name=vinoteca'><Icon iconType='category' /></Category>
+          <Category name='Limpieza' route='/categoria?name=limpieza'><Icon iconType='category' /></Category>
+          <Category name='Bebidas sin alcohol' route='/categoria?name=bebidas-sin-alcohol'><Icon iconType='category' /></Category>
+          <Category name='Bebidas con alcohol' route='/categoria?name=bebidas-con-alcohol'><Icon iconType='category' /></Category>
+          <Category name='Variados' route='/categoria?name=variados'><Icon iconType='category' /></Category>
         </nav>
       </div>
     </>
