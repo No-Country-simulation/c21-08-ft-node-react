28b3d8a46e891fd25257dc04de8eef3a15627ed3
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-14 19:04:47 -0300

    fix: prevent decimal part of prices to show 3 digits

--- site/src/app/components/Price/utils/getIntAndDecSplited.util.ts
+++ site/src/app/components/Price/utils/getIntAndDecSplited.util.ts
@@ -1,7 +1,7 @@
 export const getIntAndDecSplited = (price: number) => {
-    const [int, dec = '00'] = String(price).split('.')
+  const [int, dec = "00"] = String(price).split(".")
 
-    const formatedDec = dec.padEnd(2, '0') // Asegura que siempre tenga 2 dígitos.
+  const formatedDec = dec.padEnd(2, "0").slice(0, dec.length - 1)
 
-    return { int, dec: formatedDec }
+  return { int, dec: formatedDec }
 }
