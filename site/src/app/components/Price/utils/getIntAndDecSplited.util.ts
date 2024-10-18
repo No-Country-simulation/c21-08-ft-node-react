42a955a241b10baf611ee92c7606a1610b384ab2
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-17 21:00:33 -0300

    fix: error that caused to show only one dec fixed

--- site/src/app/components/Price/utils/getIntAndDecSplited.util.ts
+++ site/src/app/components/Price/utils/getIntAndDecSplited.util.ts
@@ -1,7 +1,7 @@
 export const getIntAndDecSplited = (price: number) => {
   const [int, dec = "00"] = String(price).split(".")
 
-  const formatedDec = dec.padEnd(2, "0").slice(0, dec.length - 1)
+  const formatedDec = dec.length > 2 ? dec.slice(0, 2) : dec.padEnd(2, "0");
 
   return { int, dec: formatedDec }
 }
