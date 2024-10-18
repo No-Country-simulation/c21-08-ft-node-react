10f871ead1242d3797792644392c9704747a6261
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-18 11:09:16 -0300

    change: card refactor, now is full responsive

--- site/src/app/components/Card/Card.component.tsx
+++ site/src/app/components/Card/Card.component.tsx
@@ -5,70 +5,61 @@ import Price from "../Price/Price.component"
 import Icon from "../Icon/Icon.component"
 import Image from "next/image"
 import useLogicCard from "@/app/hooks/useLogicCard.hook"
-import { strParseOut } from "@/app/utils/functions.utils"
+import { getProductImageAlt } from "@/app/utils/getProductImageAlt.util"
 
-const Card = ({ styleSlider, product, width }: CardProps) => {
-  const {
-    favorite,
-    handleFavorite,
-    handleAddToCart,
-    discount,
-    discountedPrice,
-  } = useLogicCard(product)
+const Card = ({ additionalStyles, product }: CardProps) => {
+  const { finalPrice, discount, isFavorite, handleFavorite, handleAddToCart } =
+    useLogicCard(product)
 
-  const { name, price, category } = product
-  let widthStyle = ""
-  if (width === "fluid") {
-    widthStyle = "w-full"
-  } else {
-    widthStyle = "max-w-[220px] max-h-[440px]"
-  }
+  const { name, price } = product
 
   return (
-    <div
-      className={`${styleSlider} flex rounded-lg ${widthStyle} flex-col bg-gray300 sm:max-w-[480px] sm:flex-row sm:items-center`}
+    <article
+      className={`flex flex-col rounded-lg bg-gray300 ${additionalStyles} max-h-[472px] min-w-0 max-w-64`}
     >
-      <div className="sm:w-1/2">
-        <Image
-          className="rounded-t-lg sm:min-w-[120px]"
-          src={"/images/product-placeholder.webp"}
-          alt={`imagen de ${name}`}
-          width={480}
-          height={480}
-        />
-      </div>
-      <div className="flex h-[180px] flex-col justify-between p-4 sm:h-full sm:justify-between sm:py-5">
-        <div className="flex">
-          <h3 className="line-clamp-2 text-lg font-bold leading-5">{name}</h3>
-          <button className="ml-auto flex" onClick={handleFavorite}>
-            <Icon iconType={`${favorite ? "heartFilled" : "heart"}`}></Icon>
+      <Image
+        className="rounded-t-lg"
+        src={"/images/product-placeholder.webp"}
+        alt={getProductImageAlt(name)}
+        width={256}
+        height={256}
+      />
+      <div className="flex flex-col p-4 xs:p-2">
+        <div className="flex justify-between">
+          <h3 className="line-clamp-3 min-h-16 max-w-48 text-[20px] font-medium leading-[20px] md:text-[16px] md:leading-[16px]">
+            {name}
+          </h3>
+          <button className="mb-auto" onClick={handleFavorite}>
+            <Icon iconType={`${isFavorite ? "heartFilled" : "heart"}`} />
           </button>
         </div>
-        <i>{strParseOut(product.brand || "")}</i>
-        {/* <h4>{category.categoryName}</h4> */}
-        {!discountedPrice ? (
-          <div className="flex h-[32px] w-full justify-start text-base font-bold">
-            <Price price={price} size={"S"}></Price>
-          </div>
-        ) : (
-          <div className="flex h-[32px] w-full justify-between">
-            <div className="flex-col items-center">
-              <div className="text-xs line-through">${price}</div>
-              <div className="text-base font-bold">
-                <Price price={discountedPrice} size={"S"}></Price>
-              </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between">
+            <div>
+              {discount && (
+                <Price
+                  price={price}
+                  size="S"
+                  additionalStyles="line-through text-gray-500"
+                />
+              )}
+              <Price price={finalPrice} size={"M"} />
             </div>
-            <div className="flex items-center">{`%${discount}`}</div>
+            {discount && (
+              <div className="flex h-8 w-16 items-center justify-center rounded-full bg-gray1000 xs:h-6 xs:w-10">
+                <span className="text-center text-[16px] font-semibold leading-[16px] text-white md:text-[14px] md:leading-[14px] xs:text-[12px] xs:font-medium xs:leading-[12px]">{`%${50}`}</span>
+              </div>
+            )}
           </div>
-        )}
-        <button
-          onClick={handleAddToCart}
-          className="mx-auto h-10 w-full rounded-lg bg-gray1000 font-bold text-white"
-        >
-          Agregar al carrito
-        </button>
+          <button
+            onClick={handleAddToCart}
+            className="h-10 w-full rounded-lg bg-gray1000 font-medium text-white xs:h-8 xs:text-[12px] xs:font-normal"
+          >
+            Agregar al carrito
+          </button>
+        </div>
       </div>
-    </div>
+    </article>
   )
 }
 
--- site/src/app/components/Card/types/CardProps.type.ts
+++ site/src/app/components/Card/types/CardProps.type.ts
@@ -1,7 +1,6 @@
 import { Product } from "../../../types/Product.type"
 
-type width = "fixed" | "fluid"
-
-export type CardProps = { product: Product } & { styleSlider?: string } & {
-  width?: width
+export type CardProps = {
+  product: Product
+  additionalStyles?: string
 }
