444e19e3d0cb07a3eceff46e59ebb18bec905ef0
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-21 09:25:43 -0300

    change: removed unused c.log

--- site/src/app/utils/ls.util.ts
+++ site/src/app/utils/ls.util.ts
@@ -2,8 +2,6 @@ import { ProductInCart } from "../components/Cart/types/ProductInCart.type"
 
 export const updateCartLS = (cartState: ProductInCart[]) => {
   window.localStorage.setItem("cart", JSON.stringify(cartState))
-  const ls = getCartItems()
-  console.log("ls: ", ls)
 }
 
 export const getCartItems = () => {
