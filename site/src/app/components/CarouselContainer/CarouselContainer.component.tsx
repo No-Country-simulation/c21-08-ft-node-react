577daaf70ba68a0943e21a6baf47d67cc4ccaaf9
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-20 22:05:55 -0300

    fix: carrousel problem with arrows fixed

--- site/src/app/components/CarouselContainer/CarouselContainer.component.tsx
+++ site/src/app/components/CarouselContainer/CarouselContainer.component.tsx
@@ -2,76 +2,76 @@
 
 import "keen-slider/keen-slider.min.css"
 import { useKeenSlider } from "keen-slider/react"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import SliderNavigationArrow from "./SliderNavigationArrow.component"
-import { DiscountContainerProps } from "./types/CarouselContainerProps.type"
+import { CarouselContainerProps } from "./types/CarouselContainerProps.type"
 import { useContext } from "react"
 import { IsClient } from "@/app/contexts/isClient.context"
 
-const CarouselContainer = ({ children }: DiscountContainerProps) => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+const CarouselContainer = ({ children }: CarouselContainerProps) => {
   const isClient = useContext(IsClient)
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const [silderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
-    slideChanged: (slider) => setCurrentSlide(slider.track.details.rel),
+    created: () => setIsLoaded(true),
+    loop: true,
     slides: {
       perView: 4,
-      spacing: 16,
+      spacing: 24,
     },
     breakpoints: {
-      "(max-width: 864px)": {
+      "(max-width: 920px)": {
         slides: {
           perView: 3,
-          spacing: 8,
+          spacing: 16,
         },
       },
       "(max-width: 584px)": {
         slides: {
           perView: 2,
-          spacing: 8,
+          spacing: 16,
         },
       },
       "(max-width: 336px)": {
         slides: {
           perView: 2,
-          spacing: 4,
+          spacing: 8,
         },
       },
     },
   })
 
-  useEffect(() => {
-    if (instanceRef.current === null) return
-    instanceRef.current.update()
-  }, [instanceRef])
-
   return (
     <div className="relative flex items-center gap-2">
-      <div className="hover:bg-dark1000 pointer-events-none absolute z-40 flex h-full w-full items-center justify-between">
+      {isLoaded && instanceRef.current && (
         <SliderNavigationArrow
           type="left"
-          onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()}
-          isDisabled={currentSlide === 0}
-        />
-        <SliderNavigationArrow
-          type="right"
-          onClick={(e) => e.stopPropagation() || instanceRef.current?.next()}
-          isDisabled={
-            instanceRef.current
-              ? currentSlide ===
-                instanceRef.current.track.details.slides.length - 1
-              : true
-          }
-        />
-      </div>
+          onClick={() => {
+            if (instanceRef.current === null) return
 
-      {isClient ? (
+            instanceRef.current.prev()
+          }}
+          isDisabled={false}
+        />
+      )}
+
+      {isClient && (
         <div ref={silderRef} className="keen-slider">
           {children}
         </div>
-      ) : (
-        ""
+      )}
+
+      {isLoaded && instanceRef.current && (
+        <SliderNavigationArrow
+          type="right"
+          onClick={() => {
+            if (instanceRef.current === null) return
+
+            instanceRef.current.next()
+          }}
+          isDisabled={false}
+        />
       )}
     </div>
   )
