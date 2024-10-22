2e5270e943c6ca76e5bd968c33dc68017c61f650
francogaray <francogaray.dev@gmail.com>
2024-10-21 21:49:29 -0300

    preparando deploy backend 3

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -5,7 +5,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.MYSQLPORT) || 3307,
+  port: (process.env.MYSQLPORT as any as number) || 3306,
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "kramer",
