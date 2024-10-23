4d7d16e69929ca5ec3b9b38ee3052e5a30d609e0
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-23 13:19:25 -0300

    change: responsive styles for addbtn

--- site/src/app/components/AddButton/AddButton.component.tsx
+++ site/src/app/components/AddButton/AddButton.component.tsx
@@ -13,7 +13,7 @@ const AddButton = ({ product, withIcon }: AddButtonProps) => {
   const { handleAddToCart } = useCardLogic(product)
   return (
     <button
-      className={`flex h-10 items-center gap-2 rounded-md bg-krOrange px-3 py-1 font-bold text-white ${withIcon ? "justify-start" : "justify-center"}`}
+      className={`flex h-10 items-center gap-2 rounded-md bg-krOrange px-3 py-1 font-bold text-white md:text-[14px] md:font-semibold md:leading-[14px] xs:text-[12px] xs:leading-[12px] xs:h-8 ${withIcon ? "justify-start" : "justify-center"}`}
       onClick={handleAddToCart}
     >
       <span>Añadir al carrito</span>
