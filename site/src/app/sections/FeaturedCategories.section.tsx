dd29bc96c5b1a3c8e4f64a6a60ef59b7fc51bbb9
GhostOrder28 <jtrvic7@gmail.com>
2024-10-23 22:58:00 -0500

    fixed: overlay was displaying behind featured categories, also a piece of unused code was removed

--- site/src/app/sections/FeaturedCategories.section.tsx
+++ site/src/app/sections/FeaturedCategories.section.tsx
@@ -3,49 +3,17 @@ import { IconProps } from "../components/Icon/types/IconProps.type"
 import { Category } from "@/app/types/Category.type"
 import { API_BASE_URL } from "@/app/consts/api.consts"
 import Icon from "../components/Icon/Icon.component"
-import {
-  removeAccents,
-  strForData,
-  strForDisplay,
-} from "../../app/utils/strFormatting.util"
+import { strForData, strForDisplay } from "../../app/utils/strFormatting.util"
 
 const getFeaturedCategories = async () => {
   const res = await fetch(`${API_BASE_URL}/category`, {
     headers: { Accept: "application/json", "Content-Type": "application/json" },
   })
   const categories: Category[] = await res.json()
+  // console.log("categories: ", categories)
   const featured = categories.filter((c) => c.featured)
-  const withImages = featured.map((c) => {
-    if (
-      removeAccents(strForData(decodeURIComponent(c.categoryName))) ===
-      "almacen"
-    ) {
-      return {
-        ...c,
-        imgUrl: "/images/almacen-featured-img.png",
-      }
-    }
-
-    if (
-      strForData(decodeURIComponent(c.categoryName)) === "bebidas-con-alcohol"
-    ) {
-      return {
-        ...c,
-        imgUrl: "/images/vinoteca-featured-img.png",
-      }
-    }
-
-    if (
-      strForData(decodeURIComponent(c.categoryName)) === "bebidas-sin-alcohol"
-    ) {
-      return {
-        ...c,
-        imgUrl: "/images/bebidas-sin-alcohol-featured-img.png",
-      }
-    }
-    return c
-  })
-  return withImages
+  // console.log("featured: ", featured)
+  return featured
 }
 
 const FeaturedCategories = async () => {
@@ -53,19 +21,24 @@ const FeaturedCategories = async () => {
   return (
     <section className="md:-mt- relative z-40 -mt-36 w-full lg:-mt-48 md:mt-0 xs:hidden">
       <div className="mx-auto flex max-w-[1000px] justify-between gap-10 lg:gap-6 lg:px-10 md:px-5 sm:gap-3">
-        {featuredCategories.map((c, idx) => (
-          <CategoryCard
-            key={`featured-category-${idx}`}
-            icon={
-              <Icon
-                iconType={`${c.categoryName}-md-color` as IconProps["iconType"]}
-                style="md:w-7"
-              />
-            }
-            name={strForDisplay(c.categoryName)}
-            link={`/categoria/${strForData(c.categoryName)}?categoryid=${c.categoryId}`}
-          />
-        ))}
+        {featuredCategories.map((c, idx) => {
+          // console.log("c: ", c)
+          return (
+            <CategoryCard
+              key={`featured-category-${idx}`}
+              icon={
+                <Icon
+                  iconType={
+                    `${c.categoryName}-md-color` as IconProps["iconType"]
+                  }
+                  style="md:w-7"
+                />
+              }
+              name={strForDisplay(c.categoryName)}
+              link={`/categoria/${strForData(c.categoryName)}?categoryid=${c.categoryId}`}
+            />
+          )
+        })}
       </div>
     </section>
   )
