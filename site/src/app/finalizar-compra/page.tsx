a2d24062422936b79b860ff9720f37925228f56c
facuso162 <facu.sosa.gabriel@gmail.com>
2024-10-28 11:30:33 -0300

    change: checkout refactor, now is responsive.

--- site/src/app/finalizar-compra/page.tsx
+++ site/src/app/finalizar-compra/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import Cart from "../components/Cart/Cart.component"
-import FormCheckout from "./components/FromCheckout.component"
-import { CheckoutProvider } from "../contexts/checkout.context"
+import { CheckoutContextProvider } from "./contexts/checkout.context"
 import { useAuth } from "../contexts/auth.context"
 import { useRouter } from "next/navigation"
 import { useContext, useEffect } from "react"
 import { CartContext } from "../contexts/cart.context"
+import CheckoutForm from "./components/CheckoutForm/CheckoutForm.component"
 
 const Checkout = () => {
   const { user } = useAuth()
@@ -13,25 +13,37 @@ const Checkout = () => {
   const { productsInCart } = useContext(CartContext)
 
   useEffect(() => {
-    if (!user && productsInCart.length != 0) {
+    if (!user) {
+      // El user no esta logeado
+      // por lo tanto, no puede
+      // ingresar a la pagina de checkout
       router.push("/auth/login")
     }
-    if (user && productsInCart.length === 0) {
+
+    // User logeado
+
+    if (productsInCart.length === 0) {
+      // No hay productos en el
+      // carrito, por lo tanto,
+      // tampoco puede ingresar a la
+      // pagina de checkout
       router.push("/")
     }
+
+    // El user tiene permisos para entrar a la pagina de checkout
   }, [user, productsInCart, router])
 
   return (
     <div className="mt-32 w-full">
       <div className="px-auto mx-auto flex min-h-[500px] max-w-[1000px] justify-center gap-1 md:flex-col">
-        <CheckoutProvider>
-          <FormCheckout></FormCheckout>
+        <CheckoutContextProvider>
+          <CheckoutForm />
           <Cart
             isCartVisible={true}
             changeCartVisibility={() => {}}
             display={"checkout"}
-          ></Cart>
-        </CheckoutProvider>
+          />
+        </CheckoutContextProvider>
       </div>
     </div>
   )
