b0a7eefdf4dda06412064a58a45b4cf6a479a4b3
GhostOrder28 <jtrvic7@gmail.com>
2024-10-31 11:51:49 -0500

    fix: correction in hero image text:

--- site/src/app/sections/Hero.section.tsx
+++ site/src/app/sections/Hero.section.tsx
@@ -4,7 +4,10 @@ const Hero = () => {
       <div className="absolute left-0 top-0 z-10 h-full w-full bg-gradient-to-t from-gray100 to-gray100/0 to-40%"></div>
       <div className="relative z-20 m-auto flex h-full max-w-[1000px] items-center justify-end">
         <h1 className="mb-14 mr-14 flex flex-col gap-5 text-6xl font-bold leading-8 tracking-tighter lg:mb-28 lg:mr-28 lg:text-5xl lg:leading-4 md:mr-28 md:mr-[10vw] md:mt-14 md:mt-28 md:text-4xl md:leading-3 sm:mr-[5vw] xs:text-2xl xs:leading-[0.2rem]">
-          <span className="text-9xl lg:text-7xl xs:text-6xl">45%</span>
+          <span className="text-4xl leading-[1px] xs:text-3xl">hasta</span>
+          <span className="-mt-4 text-9xl lg:text-8xl xs:-mt-8 xs:text-6xl">
+            15%
+          </span>
           <span>En alimentos</span>
           <span>para el hogar</span>
         </h1>
